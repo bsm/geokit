2a60b9f2a15e80d8e91c0f14ebcb250867832dfb
Son Dinh <son.dinh@blacksquaremedia.com>
2017-07-12 14:07:10 +0100

    minor typo

--- osmx/map.go
+++ osmx/map.go
@@ -41,7 +41,7 @@ func WrapMap(parent *osm.Map) (*Map, error) {
 	return m, nil
 }
 
-// CountryAlpha2 returns the ISO3166-1 alpha3 code of the Map
+// CountryAlpha2 returns the ISO3166-1 alpha2 code of the Map
 func (m *Map) CountryAlpha2() string {
 	for _, tag := range m.rel.Tags {
 		if tag.Key == "ISO3166-1:alpha2" {
