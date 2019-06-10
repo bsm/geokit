6389eab622d1c436b806869aad7b320d9525f328
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2019-06-10 12:14:46 +0100

    Expose values

--- cellstore/ext_test.go
+++ cellstore/ext_test.go
@@ -5,3 +5,6 @@ func (i *SectionIterator) BPos() int { return i.b.Pos() }
 
 // SPos returns the section position.
 func (i *SectionIterator) SPos() int { return i.s.Pos() }
+
+// Sort sorts entries by distance.
+func (n *NearbyRS) Sort() { n.sort() }
--- cellstore/nearby.go
+++ cellstore/nearby.go
@@ -8,41 +8,73 @@ import (
 	"github.com/golang/geo/s2"
 )
 
-var nearbySlicePool sync.Pool
+var nearbyRSPool sync.Pool
+
+// NearbyRS is the nearby result set.
+type NearbyRS struct {
+	Entries []NearbyEntry
+	buf     []byte
+}
+
+func newNearbyRS() *NearbyRS {
+	if v := nearbyRSPool.Get(); v != nil {
+		n := v.(*NearbyRS)
+		n.Reset()
+		return n
+	}
+	return new(NearbyRS)
+}
+
+// Len returns the number of entries in the set.
+func (n *NearbyRS) Len() int {
+	return len(n.Entries)
+}
+
+// Reset resets the set.
+func (n *NearbyRS) Reset() {
+	if n != nil {
+		n.Entries = n.Entries[:0]
+		n.buf = n.buf[:0]
+	}
+}
+
+// Release releases the result set.
+func (n *NearbyRS) Release() {
+	if n != nil {
+		nearbyRSPool.Put(n)
+	}
+}
+
+func (n *NearbyRS) add(cellID s2.CellID, value []byte, distance s1.Angle) {
+	off := len(n.buf)
+	n.buf = append(n.buf, value...)
+	n.Entries = append(n.Entries, NearbyEntry{
+		CellID:   cellID,
+		Value:    n.buf[off:len(n.buf)],
+		Distance: distance,
+	})
+}
+
+func (n *NearbyRS) sort() {
+	sort.Sort(nearbyEntrySlice(n.Entries))
+}
+
+func (n *NearbyRS) limit(limit int) {
+	if limit < len(n.Entries) {
+		n.Entries = n.Entries[:limit]
+	}
+}
 
 // NearbyEntry is returned by Nearby search.
 type NearbyEntry struct {
 	s2.CellID
+	Value    []byte
 	Distance s1.Angle
 }
 
-func makeNearbySlice(cp int) NearbySlice {
-	if v := nearbySlicePool.Get(); v != nil {
-		return v.(NearbySlice)[:0]
-	}
-	return make(NearbySlice, 0, cp)
-}
+// NearbyEntrySlice is a slice of nearby entries.
+type nearbyEntrySlice []NearbyEntry
 
-// NearbySlice is a slice of nearby entries.
-type NearbySlice []NearbyEntry
-
-func (s NearbySlice) Len() int           { return len(s) }
-func (s NearbySlice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
-func (s NearbySlice) Less(i, j int) bool { return s[i].Distance < s[j].Distance }
-
-// Release releases the slice.
-func (s NearbySlice) Release() {
-	if cap(s) != 0 {
-		nearbySlicePool.Put(s)
-	}
-}
-
-// Sort sorts the entries by distance.
-func (s NearbySlice) Sort() { sort.Sort(s) }
-
-func (s NearbySlice) limit(limit int) NearbySlice {
-	if limit < len(s) {
-		s = s[:limit]
-	}
-	return s
-}
+func (s nearbyEntrySlice) Len() int           { return len(s) }
+func (s nearbyEntrySlice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s nearbyEntrySlice) Less(i, j int) bool { return s[i].Distance < s[j].Distance }
